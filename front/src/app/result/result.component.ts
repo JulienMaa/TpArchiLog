5ff19c8f387448ecc74e7c1b79bb5a5896d8a693
Maaroufi <julien.maaroufi@gmail.com>
2024-04-09 11:55:13 +0200

    Result Rank

--- front/src/app/result/result.component.ts
+++ front/src/app/result/result.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class ResultComponent {
   title = 'Result';
+  game = "";
+  guess = "";
+  answer = "";
+  clipID: number = 0;
+
+  constructor(private route: ActivatedRoute, private sanitizer: DomSanitizer, private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.game = params['game'];
+      this.guess = params['guess'];
+      this.clipID = params['idClip'];
+      this.fetchRank();
+    });
+  }
+
+  fetchRank(): void {
+    this.http.get<any>(`http://localhost:3000/clips/${this.game}/getRankFromId/${this.clipID}`).subscribe(
+      (clip: any) => {
+        this.clipID = clip.id;
+        this.answer = clip.rank;
+      }
+    );
+  }
 }
